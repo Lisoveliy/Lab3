66c9b6e137a8d6805c2ea26c4f92550f80bf3b27
Saveliy Savenak <1986developer@gmail.com>
2023-11-21 12:08:07 +0300

    Добавьте файлы проекта.

---
+++ Calculate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Lab3
+{
+    public class Calculate
+    {
+        public Calculate(double startX, double endN)
+        {
+            this.startX = startX;
+            this.endN = endN;
+        }
+        ~Calculate() { 
+            CancelCalculations();
+        }
+        public void RunCalculations()
+        {
+            mainCancellationSource = new CancellationTokenSource();
+            mainCancellation = mainCancellationSource.Token;
+            InvokeCalculations(startX, 0, endN);
+        }
+        public void CancelCalculations()
+        {
+            mainCancellationSource.Cancel();
+        }
+        public void PauseCalculations()
+        {
+            isPaused = true;
+        }
+        public void ContinueCalculations()
+        {
+            isPaused = false;
+        }
+        public bool isPaused
+        {
+            get; private set;
+        } = false;
+        public bool isStarted
+        {
+            get; private set;
+        } = false;
+        private Task mainCalculateThread = Task.CompletedTask;
+        private CancellationTokenSource mainCancellationSource = null!;
+        private CancellationToken mainCancellation;
+        //Events
+        public EventHandler<CalculateEventArgs> CalculateStatusChanged = delegate { };
+        public EventHandler CalculationsEnded = delegate { };
+
+        private double startX { get; set; }
+        private double endN { get; set; }
+        private double _currentN;
+        public double CurrentN
+        {
+            get
+            {
+                return _currentN;
+            }
+            private set
+            {
+                _currentN = value;
+                CalculateStatusChanged.Invoke(this, new(_currentN, _currentSum));
+            }
+        }
+
+        private double _currentSum;
+        public double CurrentSum
+        {
+            get
+            {
+                return _currentSum;
+            }
+            private set
+            {
+                _currentSum = value;
+                CalculateStatusChanged.Invoke(this, new(_currentN, _currentSum));
+            }
+        }
+        private void InvokeCalculations(double startX, int startN, double endN)
+        {
+            isStarted = true;
+            mainCalculateThread = Task.Run(() =>
+            {
+                CurrentSum = 0;
+                CurrentN = endN;
+                for (CurrentN = startN; CurrentN < endN; CurrentN++)
+                {
+                    while (isPaused && !mainCancellation.IsCancellationRequested);
+                    if (mainCancellation.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    CurrentSum += Math.Pow(startX, 2 * CurrentN + 1) / (2 * CurrentN + 1);
+                }
+                CalculationsEnded.Invoke(this, new EventArgs());
+                isStarted = false;
+            }, mainCancellation);
+        }
+    }
+    public class CalculateEventArgs : EventArgs
+    {
+        public double currentX { get; set; }
+        public double currentN { get; set; }
+        public double currentSum { get; set; }
+        public CalculateEventArgs(double currentN, double currentSum)
+        {
+            this.currentN = currentN;
+            this.currentSum = currentSum;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Calculate calculate = null!;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try { 
+            double x = Convert.ToDouble(xBox.Text);
+            double n = Convert.ToDouble(nBox.Text);
+            calculate = new Calculate(x, n);
+            calculate.CalculateStatusChanged += StatusUpdated;
+            calculate.CalculationsEnded += CalculationsEnded;
+            runButton.IsEnabled = false;
+            cancelButton.IsEnabled = true;
+            calculate.RunCalculations();
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.StackTrace, ex.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            pauseButton.IsEnabled = true;
+        }
+
+        private void CalculationsEnded(object? sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                runButton.IsEnabled = true;
+                cancelButton.IsEnabled = false;
+                pauseButton.IsEnabled = false;
+                pauseButton.Content = "Преостановить";
+            });
+        }
+
+        private void StatusUpdated(object? sender, CalculateEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                statusLabel.Content =
+                "Status:\n" +
+                "Current N:" + e.currentN + "\n" +
+                "Current Sum:" + e.currentSum;
+            });
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculate.CancelCalculations();
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (calculate.isPaused)
+            {
+                calculate.ContinueCalculations();
+                pauseButton.Content = "Преостановить";
+            }
+            else
+            {
+                calculate.PauseCalculations();
+                pauseButton.Content = "Возобновить";
+            }
+        }
+    }
+}
